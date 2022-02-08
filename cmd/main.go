7238b833e1d34abeef07648d019fc8964c16b0c9
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-08 20:02:21 +0900

    remove line

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
-	"runtime"
 
 	"github.com/joho/godotenv"
 	"github.com/kenzo-takana/meander"
@@ -12,8 +11,6 @@ import (
 
 func main() {
 	// プログラムから使用できる最大にCPU数を指定できる
-	// TODO: go 1.5からはこのコードは不要
-	runtime.GOMAXPROCS(runtime.NumCPU())
 	err := godotenv.Load("../.env")
 	if err != nil {
 		panic("Error loading .env file")
