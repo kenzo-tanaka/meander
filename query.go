51052a500e46bc205d9a3eff0730630aa60e3702
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-08 19:55:15 +0900

    Add some struct to query.go

--- query.go
+++ query.go
@@ -1,3 +1,29 @@
 package meander
 
+type Place struct {
+	*googleGeometry `json:geometry`
+	Name            string          `json:"name"`
+	Icon            string          `json:"icon"`
+	Photos          []*googlePhotos `json:"photos"`
+	Vicinity        string          `json:"vicinity"`
+}
+
+type googleResponse struct {
+	Results []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	*googleLocation `json:"location"`
+}
+
+type googleLocation struct {
+	Lat float64 `josn:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhotos struct {
+	PhotoRef string `json:"photo_reference"`
+	URL      string `json:"url"`
+}
+
 var APIKey string
